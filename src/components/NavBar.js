2f12a6e96291815d6b4bf24a91973e5e37a5a3e9
Sarthak Pandey <sarthakpandey973@gmail.com>
2024-05-29 11:00:38 +0530

    refactor: Update import statement for Header component in App.js

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+// src/components/Header.js
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className="NavBar">
+        <li class="Navitem">
+          <a>Home</a>
+        </li>
+        <li class="Navitem">
+          <a>About</a>
+        </li>
+        <li class="Navitem">
+          <a>Projects</a>
+        </li>
+        <li class="Navitem">
+          <a>Contact</a>
+        </li>
+        <li class="Navitem">
+          <a>Resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
